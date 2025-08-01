b3eb58e9a3f15536cfb43ce743c5d09a68ccbf09
Kadava <kadavamail@gmail.com>
2025-08-01 16:32:57 +0100

    Updated

--- ScpProximityChat/Features/ScpProximityChatHandler.cs
+++ ScpProximityChat/Features/ScpProximityChatHandler.cs
@@ -1,6 +1,9 @@
-using System.Collections.Generic;
 using Hints;
+using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.CustomHandlers;
+using LabApi.Features.Extensions;
 using Mirror;
+using PlayerRoles;
 using PlayerRoles.FirstPersonControl;
 using PlayerRoles.Spectating;
 using PlayerRoles.Voice;
@@ -10,60 +13,83 @@ using VoiceChat.Networking;
 
 namespace ScpProximityChat.Features;
 
-public static class ScpProximityChatHandler
+public class ScpProximityChatHandler : CustomEventsHandler
 {
     public static readonly HashSet<ReferenceHub> ToggledPlayers = [];
-    
+
+    public override void OnPlayerTogglingNoclip(PlayerTogglingNoclipEventArgs ev)
+    {
+        base.OnPlayerTogglingNoclip(ev);
+
+        if (FpcNoclip.IsPermitted(ev.Player.ReferenceHub))
+            return;
+
+        
+
+        if (!ScpProximityChatModule.Config.AllowedRoles.Contains(ev.Player.Role.GetRoleBase().RoleTypeId))
+            return;
+
+        if (!ToggledPlayers.Add(ev.Player.ReferenceHub))
+        {
+            ToggledPlayers.Remove(ev.Player.ReferenceHub);
+            ev.Player.SendHint(ScpProximityChatModule.Config.ProximityChatDisabledMessage, [new StringHintParameter(string.Empty)], null, 4);
+            return;
+        }
+
+        ev.Player.SendHint(ScpProximityChatModule.Config.ProximityChatEnabledMessage, [new StringHintParameter(string.Empty)], null, 4);
+        return;
+    }
+
     public static bool OnPlayerTogglingNoClip(ReferenceHub player)
     {
         if (FpcNoclip.IsPermitted(player))
             return true;
-        
+
         if (!ScpProximityChatModule.Config.AllowedRoles.Contains(player.roleManager.CurrentRole.RoleTypeId))
             return true;
-        
+
         if (!ToggledPlayers.Add(player))
         {
             ToggledPlayers.Remove(player);
-            player.hints.Show(new TextHint(ScpProximityChatModule.Config.ProximityChatDisabledMessage, [new StringHintParameter(string.Empty)], null ,4));
+            player.hints.Show(new TextHint(ScpProximityChatModule.Config.ProximityChatDisabledMessage, [new StringHintParameter(string.Empty)], null, 4));
             return false;
         }
 
-        player.hints.Show(new TextHint(ScpProximityChatModule.Config.ProximityChatEnabledMessage, [new StringHintParameter(string.Empty)], null ,4));
+        player.hints.Show(new TextHint(ScpProximityChatModule.Config.ProximityChatEnabledMessage, [new StringHintParameter(string.Empty)], null, 4));
         return false;
     }
-    
+
     public static bool OnPlayerUsingVoiceChat(NetworkConnection connection, VoiceMessage message)
     {
         if (message.Channel != VoiceChatChannel.ScpChat)
             return true;
-        
+
         if (!ReferenceHub.TryGetHubNetID(connection.identity.netId, out ReferenceHub player))
             return true;
-        
+
         if (!ScpProximityChatModule.Config.AllowedRoles.Contains(player.roleManager.CurrentRole.RoleTypeId) || (ScpProximityChatModule.Config.ToggleChat && !ToggledPlayers.Contains(player)))
             return true;
-        
+
         SendProximityMessage(message);
         return !ScpProximityChatModule.Config.ToggleChat;
     }
-    
+
     private static void SendProximityMessage(VoiceMessage msg)
     {
         foreach (ReferenceHub referenceHub in ReferenceHub.AllHubs)
         {
-            if (referenceHub.roleManager.CurrentRole is SpectatorRole && !msg.Speaker.IsSpectatedBy(referenceHub))
+            if (referenceHub.roleManager.CurrentRole.RoleTypeId is RoleTypeId.Spectator && !msg.Speaker.IsSpectatedBy(referenceHub))
                 continue;
-                
+
             if (referenceHub.roleManager.CurrentRole is not IVoiceRole voiceRole2)
                 continue;
-            
+
             if (Vector3.Distance(msg.Speaker.transform.position, referenceHub.transform.position) >= ScpProximityChatModule.Config.MaxProximityDistance)
                 continue;
 
             if (voiceRole2.VoiceModule.ValidateReceive(msg.Speaker, VoiceChatChannel.Proximity) is VoiceChatChannel.None)
                 continue;
-            
+
             msg.Channel = VoiceChatChannel.Proximity;
             referenceHub.connectionToClient.Send(msg);
         }
--- ScpProximityChat/ScpProximityChatModule.cs
+++ ScpProximityChat/ScpProximityChatModule.cs
@@ -1,50 +1,29 @@
-﻿using HarmonyLib;
+﻿using LabApi.Events.Arguments.PlayerEvents;
+using LabApi.Events.CustomHandlers;
 using PlayerRoles;
-using PluginAPI.Core;
-using PluginAPI.Core.Attributes;
-using PluginAPI.Enums;
-using PluginAPI.Events;
 using ScpProximityChat.Features;
-using PluginConfig = ScpProximityChat.PluginConfig;
 
 namespace ScpProximityChat;
 
-public class ScpProximityChatModule
+public class ScpProximityChatModule : CustomEventsHandler
 {
-    public const string Version = "2.0.0.0";
-    
-    private static readonly Harmony HarmonyPatcher = new("scpproximitymodule.jesusqc.com");
-    
-    [PluginAPI.Core.Attributes.PluginConfig] public static PluginConfig Config;
-    
-    [PluginEntryPoint("ScpProximityChat", Version, "Makes SCPs able to talk inside the proximity chat.", "Jesus-QC")]
-    private void Init()
+    public override void OnServerRoundStarted()
     {
-        if (!Config.IsEnabled)
-            return;
-        
-        Log.Raw($"<color=blue>Loading ScpChatExtension {Version} by Jesus-QC</color>");
-        
-        HarmonyPatcher.PatchAll();
-        
-        EventManager.RegisterEvents(this);
-    }
-
-    [PluginEvent(ServerEventType.RoundRestart)]
-    public void OnRoundRestarted()
-    {
+        base.OnServerRoundStarted();
         ScpProximityChatHandler.ToggledPlayers.Clear();
     }
-    
-    [PluginEvent(ServerEventType.PlayerChangeRole)]
-    public void OnPlayerChangingRole(Player plr, PlayerRoleBase oldRole, RoleTypeId newRole, RoleChangeReason reason)
+
+    public override void OnPlayerChangingRole(PlayerChangingRoleEventArgs ev)
     {
+        base.OnPlayerChangingRole(ev);
+
         if (!Config.SendBroadcastOnRoleChange)
             return;
-        
-        if (!Config.AllowedRoles.Contains(newRole))
+
+        if (!Config.AllowedRoles.Contains(ev.NewRole))
             return;
-        
-        plr.SendBroadcast(Config.BroadcastMessage, Config.BroadcastDuration);
+
+        ev.Player.SendBroadcast(Config.BroadcastMessage, Config.BroadcastDuration);
     }
+
 }
