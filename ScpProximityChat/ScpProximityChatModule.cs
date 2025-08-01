3fc51eeb4830c9671bb2b5f82c99e0eb391e2554
Kadava <kadavamail@gmail.com>
2025-08-01 16:48:56 +0100

    Enabled and finished plugin building

--- ScpProximityChat/ScpProximityChatModule.cs
+++ ScpProximityChat/ScpProximityChatModule.cs
@@ -1,18 +1,17 @@
 ﻿using LabApi.Events.Arguments.PlayerEvents;
 using LabApi.Events.CustomHandlers;
-using PlayerRoles;
 using ScpProximityChat.Features;
 
 namespace ScpProximityChat;
 
 public class ScpProximityChatModule : CustomEventsHandler
 {
+    public static Config Config { get; set; } = new Config();
     public override void OnServerRoundStarted()
     {
         base.OnServerRoundStarted();
         ScpProximityChatHandler.ToggledPlayers.Clear();
     }
-    public static Config Config { get; set; } = new Config();
     public override void OnPlayerChangingRole(PlayerChangingRoleEventArgs ev)
     {
         base.OnPlayerChangingRole(ev);
